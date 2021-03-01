286aeeebca9db5653ee1304c4d8bf4c13e336b0f
David Billups <davidtbillups@gmail.com>
2021-03-01 14:32:53 -0500

    created nested providers

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,38 +2,58 @@ import React from "react"
 import { Route } from "react-router-dom"
 import "./Kennel.css"
 import { Home } from "./Home"
-import { AnimalCard } from "./animal/AnimalCard"
-import { EmployeeCard } from "./employee/EmployeeCard"
-import { LocationCard } from "./location/LocationCard"
-import { CustomerCard } from "./customer/CustomerCard"
+import { AnimalList } from "./animal/AnimalList"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { EmployeeList } from "./employee/EmployeeList"
+import { EmployeeProvider } from "./employee/EmployeeProvider"
+
+import { LocationList } from "./location/LocationList"
+import { LocationProvider } from "./location/LocationProvider"
+
+import { CustomerList } from "./customer/CustomerList"
+import { CustomerProvider } from "./customer/CustomerProvider"
+
 
 export const ApplicationViews = () => {
     return (
         <>
             {/* Render the location list when http://localhost:3000/ */}
+            
             <Route exact path="/">
                 <Home />
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
+            <AnimalProvider>
+                <LocationProvider>
+                    <CustomerProvider>
+                        <Route path="/animals">
+                            <AnimalList />
+                        </Route>
+                    </CustomerProvider>
+                </LocationProvider>
+            </AnimalProvider>
 
             {/* Render the employee list when http://localhost:3000/employees */}
-            <Route path="/employees">
-                <EmployeeCard />
-            </Route>
+            <EmployeeProvider>
+                <Route path="/employees">
+                    <EmployeeList />
+                </Route>
+            </EmployeeProvider>
 
             {/* Render the location list when http://localhost:3000/locations */}
-            <Route path="/locations">
-                <LocationCard />
-            </Route>
+            <LocationProvider>
+                <Route path="/locations">
+                    <LocationList />
+                </Route>
+            </LocationProvider>
 
             {/* Render the customer list when http://localhost:3000/customers */}
-            <Route path="/customers">
-                <CustomerCard />
-            </Route>
+            <CustomerProvider>
+                <Route path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
         </>
     )
 }
